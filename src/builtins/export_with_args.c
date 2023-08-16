addd720560839d73dd62f0eb108795774ca0ff15
Nicube <100804427+Nicube@users.noreply.github.com>
2023-08-16 21:28:44 +0200

    Don't export if not in value= type

--- src/builtins/export_with_args.c
+++ src/builtins/export_with_args.c
@@ -6,7 +6,7 @@
 /*   By: ndiamant <ndiamant@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 11:40:59 by ndiamant          #+#    #+#             */
-/*   Updated: 2023/08/14 23:26:14 by ndiamant         ###   ########.fr       */
+/*   Updated: 2023/08/16 21:27:09 by ndiamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,21 @@ static void	add_env(t_bash *sh, char *arg)
 	sh->envp = new_envp;
 }
 
+int	is_valid_format(char *arg)
+{
+	int	i;
+
+	if (!arg || arg[0] == '\0' || arg[0] == '=')
+		return (0);
+	i = 0;
+	while (arg[i] && arg[i] != '=')
+		i++;
+	if (arg[i] == '=')
+		return (1);
+	return (0);
+}
+
+
 void	export_cmd(t_bash *sh, char *arg)
 {
 	char	*key;
@@ -73,7 +88,8 @@ void	export_cmd(t_bash *sh, char *arg)
 	env_index = find_key(sh, key);
 	if (env_index >= 0)
 		unset_cmd(sh, key);
-	add_env(sh, arg);
+	if (is_valid_format(arg))
+		add_env(sh, arg);
 	free(key);
 }
 
